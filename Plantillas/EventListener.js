530d1099faace6fe70d7e0c888d261d4a9946eff
Alejandro <alejandrosanrod@outlook.com>
2023-10-21 10:36:38 +0200

    Cambio de proyecto nuevo -Web de juegos

--- Plantillas/EventListener.js
+++
@@ -1,6 +0,0 @@
-const MenuICon = document.querySelector('.Menu-Icon')
-const MenuFadeDown = document.querySelector('.Menu')
-MenuICon.addEventListener('click',(e)=>{
-    MenuICon.classList.toggle('open')
-    MenuFadeDown.classList.toggle('showMenu')
-})
